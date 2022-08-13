62023875f84e25df0c770b93ce67bf4893fa0e7c
SergiiPanchuk <sergiip1994@gmail.com>
2022-08-13 15:57:52 +0000

    add js burger

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,11 @@
 let menuBtn = document.querySelector('.burger');
-
-menuBtn.addEventListener('click', function(){
-    menuBtn.classList.toggle('active');
-})
+let burgerLinks = document.querySelector('.burger__links');
+let linksActive = document.querySelector('.burger__links');
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active')
+    burgerLinks.classList.toggle('burger__links-active')
+});
+linksActive.addEventListener('click', function () {
+    menuBtn.classList.remove('active')
+    burgerLinks.classList.remove('burger__links-active')
+});
