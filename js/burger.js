bc419c56bee312ac96a8fa870da82ad109d8d240
SergiiPanchuk <sergiip1994@gmail.com>
2022-08-26 15:32:36 +0000

    add iifi

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,12 @@
+(function () {
 let menuBtn = document.querySelector('.burger');
 let burgerLinks = document.querySelector('.burger__links');
-let linksActive = document.querySelector('.burger__links');
 menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('active')
     burgerLinks.classList.toggle('burger__links-active')
 });
-linksActive.addEventListener('click', function () {
+burgerLinks.addEventListener('click', function () {
     menuBtn.classList.remove('active')
     burgerLinks.classList.remove('burger__links-active')
 });
+})();
